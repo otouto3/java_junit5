efa634adc92654b4ed0574ab3fe1850f7c46649b
otouto3 <otoutoyeah@gmail.com>
2025-08-14 23:25:49 +0900

    test: 通貨の概念の導入
    - 1回目のgreenまで
    
    p56の実装

--- src/main/java/tddbc/Money.java
+++ src/main/java/tddbc/Money.java
@@ -3,6 +3,7 @@ package tddbc;
 abstract class Money {
     protected int amount;
     abstract Money times(int multiplier);
+    abstract String currency();
 
     public boolean equals(Object object) {
         Money money = (Money) object;

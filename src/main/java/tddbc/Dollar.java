7f479314b5892b7cff79fb68f7d452f11292e44e
otouto3 <otoutoyeah@gmail.com>
2025-08-14 15:20:22 +0900

    test: $5 * 2 = $10のテストのリファクタリング
    Dollarクラスの骨組みを実装：
    - 5 * 2の重複を排除
    
    テスト駆動開発 p.10の実装

--- src/main/java/tddbc/Dollar.java
+++ src/main/java/tddbc/Dollar.java
@@ -3,10 +3,10 @@ package tddbc;
 class Dollar {
     int amount;
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-
+        amount *= multiplier;
     }
 }
